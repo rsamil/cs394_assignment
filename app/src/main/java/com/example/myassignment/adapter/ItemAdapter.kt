2480db91dbccd913d53a5983e9d274624441b4c3
rsamil <127696173+rsamil@users.noreply.github.com>
2024-11-08 03:06:31 +0300

    CS394 data section finished

--- app/src/main/java/com/example/myassignment/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/myassignment/adapter/ItemAdapter.kt
@@ -4,10 +4,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
+
 import com.example.myassignment.model.Game
 import com.example.myassignment.R
 
-class ItemAdapter(private val data: List<Game>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+class ItemAdapter(private val data: List<Game>) {
 
 }
